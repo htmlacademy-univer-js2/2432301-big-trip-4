9e72262b64007c44c3e5bf8b87c6b67e92faedd2
Alex-Vay <vatlin04@mail.ru>
2024-04-11 01:17:14 +0500

    4.15. Шаблонизируй то

--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
-import TripInfoView from './view/trip-info-view.js';
-import FiltersView from './view/filters-view.js';
-import { RenderPosition, render } from './render.js';
+// import TripInfoView from './view/trip-info-view.js';
+// import FiltersView from './view/filters-view.js';
+// import { RenderPosition, render } from './render.js';
 import Presenter from './presenter/presenter.js';
 import PointModel from './model/point-model.js';
 
-const pageMainElement = document.querySelector('.trip-main');
-const filterElement = document.querySelector('.trip-controls__filters');
+// const pageMainElement = document.querySelector('.trip-main');
+// const filterElement = document.querySelector('.trip-controls__filters');
 const tripEventsElement = document.querySelector('.trip-events');
 
 const pointModel = new PointModel();
 
-render(new TripInfoView(), pageMainElement, RenderPosition.AFTERBEGIN);
-render(new FiltersView(), filterElement);
+// render(new TripInfoView(), pageMainElement, RenderPosition.AFTERBEGIN);
+// render(new FiltersView(), filterElement);
 
 const tripPresenter = new Presenter({
   container: tripEventsElement,
