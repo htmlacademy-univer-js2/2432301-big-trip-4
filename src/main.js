b08421d81eea5cdaa12130988c89bcd3d6a7b01e
Alex-Vay <vatlin04@mail.ru>
2024-05-19 20:38:36 +0500

    module6-task2

--- src/main.js
+++ src/main.js
@@ -3,14 +3,14 @@ import Presenter from './presenter/presenter.js';
 import PointModel from './model/point-model.js';
 import { generateFilter } from './mock/filters.js';
 import FiltersView from './view/filters-view.js';
-import { getRandomPoint } from './mock/offer-point-town.js';
+import { getRandomPoints } from './mock/offer-point-town.js';
 import TripInfoView from './view/trip-info-view.js';
 
 
 const filtersContainer = document.querySelector('.trip-controls__filters');
 const tripContainer = document.querySelector('.trip-events');
 const pointModel = new PointModel();
-pointModel.init(getRandomPoint());
+pointModel.init(getRandomPoints());
 const tripPresenter = new Presenter(tripContainer, pointModel);
 tripPresenter.init();
 const filters = generateFilter(pointModel.points);
